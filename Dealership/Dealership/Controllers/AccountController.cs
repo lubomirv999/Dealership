9e4f8303a6b9965d69e907aa5877e1df0e443973
Lubo <lubomir999@abv.bg>
2018-09-19 13:26:59 +0300

    Fixed Some Creating User Minor Bugs
    Fixed Some Creating User Minor Bugs

--- Dealership/Dealership/Controllers/AccountController.cs
+++ Dealership/Dealership/Controllers/AccountController.cs
@@ -202,6 +202,7 @@
 
         [HttpGet]
         [AllowAnonymous]
+        [Authorize]
         public IActionResult Register(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
@@ -211,6 +212,7 @@
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
@@ -220,14 +222,6 @@
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
-
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    _logger.LogInformation("User created a new account with password.");
                     return RedirectToAction("AllCars", "Car");
                 }
                 AddErrors(result);
--- Dealership/Dealership/Controllers/HomeController.cs
+++ Dealership/Dealership/Controllers/HomeController.cs
@@ -9,21 +9,7 @@
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
+        }        
 
         public IActionResult Error()
         {
